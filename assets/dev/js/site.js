3a0679fcf0a07157483d3cfba62594572bdd23e8
Dan Klotz <danferth@gmail.com>
2016-04-22 00:37:17 +0000

    rev checkbox working, product sort a little buggy right now

--- assets/dev/js/site.js
+++ assets/dev/js/site.js
@@ -4,10 +4,12 @@ app.controller('catalog', ['$scope', '$http', function($scope, $http){
     $http.get('assets/build/json/fliers.json').then(function(res){
         $scope.data = res.data;
         
-        $scope.thActive = true;
-        $scope.listActive = false;
+        $scope.thActive = false;
+        $scope.listActive = true;
         
         $scope.cardHovered = false;
+        
+        $scope.sortBy = "data.section";
         
     });
 }]);
